5e7a13506997585e7021591424c923f504f75520
minnnisu <mine52100@naver.com>
2024-02-16 19:16:37 +0900

    init setting

---
+++ src/main/java/minnnisu/personalnote/PersonalNoteApplication.java
@@ -0,0 +1,13 @@
+package minnnisu.personalnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minnnisu/personalnote/PersonalNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package minnnisu.personalnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
