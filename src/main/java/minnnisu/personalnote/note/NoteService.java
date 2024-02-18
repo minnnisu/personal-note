32447f109c1bea780d72c231dda688fca2698915
minnnisu <mine52100@naver.com>
2024-02-18 19:43:50 +0900

    Add services

---
+++ src/main/java/minnnisu/personalnote/note/NoteService.java
@@ -0,0 +1,47 @@
+package minnnisu.personalnote.note;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import minnnisu.personalnote.user.User;
+import minnnisu.personalnote.user.UserNotFoundException;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NoteService {
+
+    private final NoteRepository noteRepository;
+
+    public List<Note> findByUser(User user) {
+        if(user == null){
+            throw new UserNotFoundException();
+        }
+        if(user.isAdmin()){
+            noteRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+        }
+        return noteRepository.findByUserOrderByIdDesc(user);
+    }
+
+    public Note saveNote(User user, String title, String content) {
+       if(user == null) {
+           throw new UserNotFoundException();
+       }
+
+       return noteRepository.save(new Note(title, content, user));
+    }
+
+    public void deleteNote(User user, Long noteId) {
+       if(user == null){
+           throw new UserNotFoundException();
+        }
+
+       Note note = noteRepository.findByIdAndUser(noteId, user);
+       if (note != null){
+           noteRepository.delete(note);
+       }
+    }
+}
---
+++ src/main/java/minnnisu/personalnote/notice/NoticeService.java
@@ -0,0 +1,30 @@
+package minnnisu.personalnote.notice;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class NoticeService {
+
+    private final NoticeRepository noticeRepository;
+
+    @Transactional(readOnly = true)
+    public List<Notice> findAll() {
+        return noticeRepository.findAll(Sort.by(Direction.DESC, "id"));
+    }
+
+    public Notice saveNotice(String title, String content) {
+        return noticeRepository.save(new Notice(title, content));
+    }
+
+    public void deleteNotice(Long id) {
+        noticeRepository.findById(id).ifPresent(noticeRepository::delete);
+    }
+}
