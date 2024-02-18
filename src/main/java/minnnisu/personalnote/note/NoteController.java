001741a312c5627edd80e2c04380e7f80b242932
minnnisu <mine52100@naver.com>
2024-02-18 19:43:23 +0900

    Add controllers

---
+++ src/main/java/minnnisu/personalnote/note/NoteController.java
@@ -0,0 +1,41 @@
+package minnnisu.personalnote.note;
+
+import lombok.RequiredArgsConstructor;
+import minnnisu.personalnote.user.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/note")
+@RequiredArgsConstructor
+public class NoteController {
+    private final NoteService noteService;
+
+    @GetMapping
+    public String getNote(Authentication authentication, Model model) {
+        User user = (User) authentication.getPrincipal();
+        List<Note> notes = noteService.findByUser(user);
+        model.addAttribute("notes", notes);
+
+        return "note/index";
+    }
+
+    @PostMapping
+    public String saveNote(Authentication authentication, @ModelAttribute NoteRegisterDto noteDto){
+        User user = (User) authentication.getPrincipal();
+        noteService.saveNote(user, noteDto.getTitle(), noteDto.getContent());
+        return "redirect:note";
+    }
+
+    @DeleteMapping
+    public String deleteNote(Authentication authentication, @RequestParam Long id){
+        User user = (User) authentication.getPrincipal();
+        noteService.deleteNote(user, id);
+        return "redirect:note";
+    }
+
+}
