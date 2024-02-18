64f66a711dca94da3ee54496d50626a2f7b6e29b
minnnisu <mine52100@naver.com>
2024-02-18 19:44:05 +0900

    Add dtos

---
+++ src/main/java/minnnisu/personalnote/note/NoteRegisterDto.java
@@ -0,0 +1,11 @@
+package minnnisu.personalnote.note;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class NoteRegisterDto {
+    private String title;
+    private String content;
+}
