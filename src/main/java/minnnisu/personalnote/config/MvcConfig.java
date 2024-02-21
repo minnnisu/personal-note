73c4308d817b2108d7e276e1ddbf0699353d43b8
minnnisu <mine52100@naver.com>
2024-02-21 13:01:19 +0900

    Update config

--- src/main/java/minnnisu/personalnote/config/MvcConfig.java
+++ src/main/java/minnnisu/personalnote/config/MvcConfig.java
@@ -11,8 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/home").setViewName("index");
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
     }
 }
