5faf1264fc74fb1acb3ded86652801d80e536c5e
minnnisu <mine52100@naver.com>
2024-02-23 11:22:01 +0900

    Update dto

--- src/main/java/minnnisu/personalnote/dto/SignupResponseDto.java
+++ src/main/java/minnnisu/personalnote/dto/SignupResponseDto.java
@@ -1,28 +1,17 @@
 package minnnisu.personalnote.dto;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.Setter;
 import minnnisu.personalnote.domain.User;
-
-import java.util.Map;
 
 @Getter
 @Setter
 public class SignupResponseDto {
     private String message;
-    private UserInfo userInfo;
-
-    public SignupResponseDto(){}
+    private UserInformation userInformation;
 
     public SignupResponseDto(User user){
         this.message = "회원가입에 성공하였습니다.";
-        this.userInfo = new UserInfo(user.getUsername(), user.getName(), user.getEmail());
+        this.userInformation = new UserInformation(user.getUsername(), user.getName(), user.getEmail());
     }
-}
-
-@Getter
-@AllArgsConstructor
-class UserInfo {
-    private final String username;
-    private final String name;
-    private final String email;
 }
--- src/main/java/minnnisu/personalnote/dto/UserInformation.java
+++ src/main/java/minnnisu/personalnote/dto/UserInformation.java
@@ -3,26 +3,10 @@ package minnnisu.personalnote.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-public class UserInfo {
+@Getter
+@AllArgsConstructor
+public class UserInformation {
     public String username;
     public String name;
     public String email;
-
-    public UserInfo(String username, String name, String email){
-        this.username = username;
-        this.name = name;
-        this.email = email;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
 }
