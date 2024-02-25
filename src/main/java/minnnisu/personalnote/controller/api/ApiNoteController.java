62ed3e76b59dd98c4ce1fc0b1b2ee5b6111d3d37
minnnisu <mine52100@naver.com>
2024-02-25 20:02:08 +0900

    Modify ApiNoteController
    쿼리 파라미터 @AuthenticationPrincipal로 변경

--- src/main/java/minnnisu/personalnote/controller/api/ApiNoteController.java
+++ src/main/java/minnnisu/personalnote/controller/api/ApiNoteController.java
@@ -1,6 +1,7 @@
 package minnnisu.personalnote.controller.api;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import minnnisu.personalnote.domain.User;
 import minnnisu.personalnote.dto.note.NoteResponseDto;
 import minnnisu.personalnote.dto.note.NoteDto;
@@ -9,26 +10,27 @@ import minnnisu.personalnote.service.NoteService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
+@Slf4j
 @RequestMapping("/api/note")
 @RequiredArgsConstructor
 public class ApiNoteController {
     private final NoteService noteService;
 
     @PostMapping
-    public ResponseEntity<NoteResponseDto> saveNote(Authentication authentication, @Valid @RequestBody NoteRequestDto noteRequestDto){
-        User user = (User) authentication.getPrincipal();
+    public ResponseEntity<NoteResponseDto> saveNote(@AuthenticationPrincipal User user, @Valid @RequestBody NoteRequestDto noteRequestDto){
         NoteDto noteDto = noteService.saveNote(user, noteRequestDto);
         return new ResponseEntity<>(NoteResponseDto.fromDto(noteDto), HttpStatus.CREATED);
     }
 
     @DeleteMapping
-    public ResponseEntity<?> deleteNote(Authentication authentication, @RequestParam Long id){
-        User user = (User) authentication.getPrincipal();
+    public ResponseEntity<?> deleteNote(@AuthenticationPrincipal User user, @RequestParam Long id){
         noteService.deleteNote(user, id);
         return new ResponseEntity<>("노트를 성공적으로 삭제하였습니다", HttpStatus.OK);
 
