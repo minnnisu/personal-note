c4a7ae35a608940ebc342ad75561fe385feb8d19
minnnisu <mine52100@naver.com>
2024-03-01 16:16:37 +0900

    Add test

--- src/test/java/minnnisu/personalnote/controller/AdminControllerTest.java
+++ src/test/java/minnnisu/personalnote/controller/AdminControllerTest.java
@@ -1,65 +1,44 @@
 package minnnisu.personalnote.controller;
 
-import minnnisu.personalnote.domain.User;
-import minnnisu.personalnote.repository.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
+import minnnisu.personalnote.service.NoteService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@SpringBootTest
-@ActiveProfiles(profiles = "test")
-@Transactional
+@WebMvcTest(AdminController.class)
 class AdminControllerTest {
+    private final MockMvc mockMvc;
 
-    @Autowired
-    private UserRepository userRepository;
-    private MockMvc mockMvc;
-    private User user;
-    private User admin;
+    @MockBean
+    NoteService noteService;
 
-    @BeforeEach
-    public void setUp(@Autowired WebApplicationContext applicationContext) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext)
-                .apply(springSecurity()) // spring security 적용
-                .alwaysDo(print())
-                .build();
-        // ROLE_USER 권한이 있는 유저 생성
-        user = userRepository.save(new User("user", "user", "ROLE_USER"));
-        // ROLE_ADMIN 권한이 있는 관리자 생성
-        admin = userRepository.save(new User("admin", "admin", "ROLE_ADMIN"));
+    public AdminControllerTest(@Autowired MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
     }
 
     @Test
-    void getNoteForAdmin_인증없음() throws Exception {
-        mockMvc.perform(get("/admin").with(csrf())) // csrf 토큰 추가
-                .andExpect(redirectedUrlPattern("**/login"))
-                .andExpect(status().is3xxRedirection()); // login이 안되있으므로 로그인 페이지로 redirect
-    }
+    @WithMockUser
+    void givenNothing_whenRequestingAdminPage_thenReturnsIndexPage() throws Exception {
+        // Given
+        given(noteService.findAll()).willReturn(List.of());
 
-    @Test
-    void getNoteForAdmin_어드민인증있음() throws Exception {
-        mockMvc.perform(get("/admin").with(csrf()).with(user(admin))) // 어드민 추가
-                .andExpect(status().is2xxSuccessful());
-    }
-
-    @Test
-    void getNoteForAdmin_유저인증있음() throws Exception {
-        mockMvc.perform(get("/admin").with(csrf()).with(user(user))) // 유저 추가
-                .andExpect(status().isForbidden()); // 접근 거부
+        // When & Then
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("admin/index"))
+                .andExpect(model().hasNoErrors())
+                .andExpect(model().attributeExists("notes"));
     }
 }
