e219ec7faf6707641a5da37e5f590f538a37b437
minnnisu <mine52100@naver.com>
2024-03-01 13:27:08 +0900

    Add notice api controller

---
+++ src/main/java/minnnisu/personalnote/controller/api/ApiNoticeController.java
@@ -0,0 +1,34 @@
+package minnnisu.personalnote.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import minnnisu.personalnote.dto.notice.NoticeDeleteResponseDto;
+import minnnisu.personalnote.dto.notice.NoticeDto;
+import minnnisu.personalnote.dto.notice.NoticeRequestDto;
+import minnnisu.personalnote.dto.notice.NoticeSaveResponseDto;
+import minnnisu.personalnote.service.NoticeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/notice")
+@RequiredArgsConstructor
+public class ApiNoticeController {
+    private final NoticeService noticeService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping
+    public ResponseEntity<NoticeSaveResponseDto> saveNotice(NoticeRequestDto noticeRequestDto) {
+        NoticeDto noticeDto = noticeService.saveNotice(noticeRequestDto);
+        return new ResponseEntity<>(NoticeSaveResponseDto.fromNoticeDto(noticeDto), HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping
+    public ResponseEntity<NoticeDeleteResponseDto> deleteNotice(@RequestParam Long noticeId) {
+        noticeService.deleteNotice(noticeId);
+        return new ResponseEntity<>(new NoticeDeleteResponseDto(), HttpStatus.OK);
+    }
+
+}
