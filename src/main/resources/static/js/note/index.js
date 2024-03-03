78b6aa8e7424205eececa70fc943a19167dfb611
minnnisu <mine52100@naver.com>
2024-03-03 19:56:42 +0900

    Modify note index page

--- src/main/resources/static/js/note/index.js
+++ src/main/resources/static/js/note/index.js
@@ -5,11 +5,11 @@ deleteBtns.forEach(deleteBtn => {
         const noteContainerNode = e.target.parentNode.parentNode.parentNode
         const noteId = noteContainerNode.getAttribute("value");
 
-        const response = await fetch(`http://localhost:8080/api/note?id=${noteId}`,{
+        const response = await fetch(`http://localhost:8080/api/note?id=${noteId}`, {
             method: "DELETE"
         })
 
-        if(!response.ok){
+        if (!response.ok) {
             const responseBody = await response.json();
             return alert(responseBody.message);
         }
@@ -18,33 +18,3 @@ deleteBtns.forEach(deleteBtn => {
         window.location.reload();
     })
 })
-
-async function saveNote() {
-    const title = document.getElementById("note-input-modal-title").value;
-    const content = document.getElementById("note-input-modal-content").value;
-
-    const requestBody = {
-        title: title,
-        content: content
-    }
-
-    console.log(requestBody)
-
-    const response = await fetch("http://localhost:8080/api/note", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    });
-
-    if (!response.ok) {
-        const responseBody = await response.json();
-        alert(responseBody)
-        return;
-    }
-
-    alert("성공적으로 노트를 추가하였습니다.");
-    window.location.href = "/note"
-
-}
