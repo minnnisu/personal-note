a4db04c307d5e4c39aa50d184fc465cd88e71c7c
minnnisu <mine52100@naver.com>
2024-03-04 19:42:01 +0900

    Modify dto by Adding NoteImage

--- src/main/java/minnnisu/personalnote/dto/note/NoteUserDto.java
+++ src/main/java/minnnisu/personalnote/dto/note/NoteUserDto.java
@@ -2,8 +2,10 @@ package minnnisu.personalnote.dto.note;
 
 import lombok.*;
 import minnnisu.personalnote.domain.Note;
+import minnnisu.personalnote.domain.NoteImage;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -17,6 +19,7 @@ public class NoteUserDto {
     private String content;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+    private List<String> imageName;
 
     public static NoteUserDto fromEntity(Note note) {
         return NoteUserDto.builder()
@@ -25,8 +28,21 @@ public class NoteUserDto {
                 .content(note.getContent())
                 .createdAt(note.getCreatedAt())
                 .updatedAt(note.getUpdatedAt())
+                .imageName(List.of())
                 .build();
     }
+
+    public static NoteUserDto fromEntity(Note note, List<NoteImage> noteImages) {
+        return NoteUserDto.builder()
+                .id(note.getId())
+                .title(note.getTitle())
+                .content(note.getContent())
+                .createdAt(note.getCreatedAt())
+                .updatedAt(note.getUpdatedAt())
+                .imageName(noteImages.stream().map(NoteImage::getImageName).toList())
+                .build();
+    }
+
 
     public static NoteUserDto of(
             Long id,
@@ -41,6 +57,7 @@ public class NoteUserDto {
                 .content(content)
                 .createdAt(createdAt)
                 .updatedAt(updatedAt)
+                .imageName(List.of())
                 .build();
     }
 
