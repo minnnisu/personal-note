10e30b820ea8dcae195fd80fb951a3e2ecfc34ed
minnnisu <mine52100@naver.com>
2024-03-05 23:06:26 +0900

    Modify dto to resize a content

--- src/main/java/minnnisu/personalnote/dto/note/NoteSummaryDto.java
+++ src/main/java/minnnisu/personalnote/dto/note/NoteSummaryDto.java
@@ -14,12 +14,14 @@ import java.time.LocalDateTime;
 public class NoteSummaryDto {
     private Long id;
     private String title;
+    private String content;
     private LocalDateTime createdAt;
 
     public static NoteSummaryDto fromEntity(Note note) {
         return NoteSummaryDto.builder()
                 .id(note.getId())
                 .title(note.getTitle())
+                .content(resizeContent(note.getContent()))
                 .createdAt(note.getCreatedAt())
                 .build();
     }
@@ -27,12 +29,23 @@ public class NoteSummaryDto {
     public static NoteSummaryDto of(
             Long id,
             String title,
+            String content,
             LocalDateTime createdAt
     ) {
         return NoteSummaryDto.builder()
                 .id(id)
                 .title(title)
+                .content(resizeContent(content))
                 .createdAt(createdAt)
                 .build();
+    }
+
+
+    static String resizeContent(String content) {
+        if (content.length() > 15) {
+            content = content.substring(0, 15) + "...";
+        }
+
+        return content;
     }
 }
