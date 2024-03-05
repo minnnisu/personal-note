9300da31897e55c8f65cac44dbf4c2c48cf7174e
minnnisu <mine52100@naver.com>
2024-03-05 23:07:16 +0900

    Add note register page

--- src/main/resources/static/js/note/register.js
+++ src/main/resources/static/js/note/register.js
@@ -1,29 +1,114 @@
+const MAX_IMAGE_COUNT = 10;
+
+function selectProductImg() {
+    const inputElement = document.getElementById("imageInput");
+
+    // input 태그를 클릭
+    inputElement.click();
+}
+
+function addImages() {
+    const input = document.getElementById("imageInput");
+
+    const files = input.files;
+    const preview = document.getElementById("imagePreview");
+
+    const totalImageCount = preview.childElementCount + files.length;
+    // 카메라 버튼 포함하고 계산해야 되므로 +1 한다.
+    if (totalImageCount > MAX_IMAGE_COUNT + 1) {
+        return alert("이미지는 최대 10개까지만 등록할 수 있습니다");
+    }
+
+    for (const file of files) {
+        const reader = new FileReader();
+
+        reader.onload = function (e) {
+            const previewImage = document.createElement("img");
+            previewImage.className = "preview_image";
+            previewImage.src = e.target.result;
+            previewImage.imageFile = file;
+
+            const deleteBtnWrapper = document.createElement("button");
+            deleteBtnWrapper.className = "delete_btn_wrapper";
+
+            const deleteBtn = document.createElement("span");
+            deleteBtn.className = "delete_btn material-symbols-outlined";
+            deleteBtn.textContent = "close";
+            deleteBtnWrapper.appendChild(deleteBtn);
+
+            deleteBtnWrapper.onclick = function () {
+                removeImage(previewImage);
+            };
+
+            const previewImageContainer = document.createElement("div");
+            previewImageContainer.className = "preview_image_container";
+            previewImageContainer.appendChild(previewImage);
+            previewImageContainer.appendChild(deleteBtnWrapper);
+
+            preview.appendChild(previewImageContainer);
+        };
+
+        reader.readAsDataURL(file); // base64로 변환
+    }
+}
+
+function removeImage(previewImage) {
+    const preview = document.getElementById("imagePreview");
+    preview.removeChild(previewImage.parentNode);
+}
+
+function checkProductDataInputValid(data) {
+    const {title, content, imageCount} = data;
+
+    if (title === "") {
+        throw new Error("제목을 입력해주세요.");
+    }
+
+    if (title.length > 20) {
+        throw new Error("제목은 최대 20자까지 입력 가능합니다.")
+    }
+
+    if (content.trim() === "") {
+        throw new Error("본문을 입력해주세요.");
+    }
+}
+
 async function saveNote() {
-    const title = document.getElementById("title").value;
+    const formData = new FormData();
+    const preview = document.getElementById("imagePreview");
+
+    const title = document.getElementById("title").value.trim(); // 앞, 뒤 공백 제거
     const content = document.getElementById("content").value;
 
-    const requestBody = {
-        title: title,
-        content: content
+    try {
+        checkProductDataInputValid({
+            title,
+            content,
+            imageCount: preview.childNodes.length - 3,
+        });
+
+        for (let i = 3; i < preview.childNodes.length; i++) {
+            const imgContainer = preview.childNodes[i];
+            formData.append("files", imgContainer.childNodes[0].imageFile);
+        }
+
+        const json = JSON.stringify({title, content})
+        const blob = new Blob([json], {type: "application/json"});
+        formData.append("note", blob);
+
+        const response = await fetch("http://localhost:8080/api/note", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            return alert(data.message);
+        }
+
+        window.location.href = `/note/${data.id}`;
+    } catch (error) {
+        return alert(error.message || "오류가 발생하였습니다");
     }
-
-    console.log(requestBody)
-
-    const response = await fetch("http://localhost:8080/api/note", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    });
-
-    if (!response.ok) {
-        const responseBody = await response.json();
-        alert(responseBody)
-        return;
-    }
-
-    alert("성공적으로 노트를 추가하였습니다.");
-    window.location.href = "/note"
-
 }
