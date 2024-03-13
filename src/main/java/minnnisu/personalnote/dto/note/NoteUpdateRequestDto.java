9140b037c8358db197ebbd34957b3e9ff520e960
minnnisu <mine52100@naver.com>
2024-03-13 19:36:15 +0900

    Modify dto

--- src/main/java/minnnisu/personalnote/dto/note/NoteUpdateRequestDto.java
+++ src/main/java/minnnisu/personalnote/dto/note/NoteUpdateRequestDto.java
@@ -2,6 +2,8 @@ package minnnisu.personalnote.dto.note;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
@@ -10,7 +12,10 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class NoteUpdateRequestDto {
+    @NotBlank(message = "제목을 입력해주세요.")
+    @Size(max = 20, message = "최대 20자까지 입력 가능합니다.")
     private String title;
+    @NotBlank(message = "본문을 작성해세요.")
     private String content;
     private List<String> deletionTargetImages;
 
--- src/main/java/minnnisu/personalnote/dto/note/NoteUpdateResponseDto.java
+++ src/main/java/minnnisu/personalnote/dto/note/NoteUpdateResponseDto.java
@@ -2,6 +2,7 @@ package minnnisu.personalnote.dto.note;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -10,8 +11,21 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 public class NoteUpdateResponseDto {
+    private Long id;
     private String title;
     private String content;
-    private List<String> deletedImages;
-    private List<String> createdImages;
+    private List<String> imageNames;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public static NoteUpdateResponseDto fromDto(NoteUserDto noteUserDto) {
+        return NoteUpdateResponseDto.builder()
+                .id(noteUserDto.getId())
+                .title(noteUserDto.getTitle())
+                .content(noteUserDto.getContent())
+                .imageNames(noteUserDto.getImageName())
+                .createdAt(noteUserDto.getCreatedAt())
+                .updatedAt(noteUserDto.getUpdatedAt())
+                .build();
+    }
 }
