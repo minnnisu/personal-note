a205056b91c5a1bf7b0e3d785c79fd38701fbd51
minnnisu <mine52100@naver.com>
2024-03-13 19:51:59 +0900

    Refactor

--- src/main/java/minnnisu/personalnote/service/FileService.java
+++ src/main/java/minnnisu/personalnote/service/FileService.java
@@ -20,20 +20,11 @@ public class FileService {
         return file.getContentType() != null && file.getContentType().startsWith("image");
     }
 
-    public String saveNoteImage(MultipartFile file) throws Exception {
+    public String saveFiles(UploadPathType uploadPathType, MultipartFile file) throws Exception {
         String uniqueFileName = UniqueFileNameGenerator.generateUniqueFileName(Objects.requireNonNull(file.getOriginalFilename()));
-        Path filePath = Paths.get(UploadPathType.IMAGE.getPath() + "/" + uniqueFileName);
+        Path filePath = Paths.get(uploadPathType.getPath() + "/" + uniqueFileName);
         Files.copy(file.getInputStream(), filePath);
         return uniqueFileName;
-    }
-
-    public void onSaveNoteFailure(String imageName) {
-        String filePath = UploadPathType.IMAGE.getPath() + "/" + imageName;
-        File file = new File(filePath);
-        if (!file.delete()) {
-            log.info("노트 이미지 파일의 삭제에 실패하였습니다.");
-            log.info("이미지 위치:" + filePath);
-        }
     }
 
     public void deleteFiles(UploadPathType uploadPathType, List<String> fileNames) {
--- src/main/java/minnnisu/personalnote/service/NoteService.java
+++ src/main/java/minnnisu/personalnote/service/NoteService.java
@@ -45,50 +45,6 @@ public class NoteService {
                 .toList();
     }
 
-    public NoteUserDto saveNote(User user, NoteSaveRequestDto noteSaveRequestDto, List<MultipartFile> files) {
-        List<NoteImage> noteImages = new ArrayList<>();
-
-        // 노트 저장
-        Note note = noteRepository.save(new Note(
-                noteSaveRequestDto.getTitle(),
-                noteSaveRequestDto.getContent(),
-                user
-        ));
-
-        // 스토리지 및 데이터베이스에 노트 이미지 저장
-        if (files != null) {
-            for (MultipartFile file : files) {
-                if (!fileService.checkImageFile(file)) {
-                    onSaveNoteFailure(note, noteImages);
-                    throw new CustomErrorException(ErrorCode.NoImageFileError);
-                }
-
-                try {
-                    String uniqueFileName = fileService.saveNoteImage(file);
-                    noteImages.add(noteImageRepository.save(new NoteImage(note, uniqueFileName)));
-                } catch (NullPointerException e) {
-                    onSaveNoteFailure(note, noteImages);
-                    throw new CustomErrorException(ErrorCode.NoImageNameError);
-
-                } catch (Exception e) {
-                    onSaveNoteFailure(note, noteImages);
-                    throw new CustomErrorException(ErrorCode.InternalServerError);
-                }
-            }
-
-        }
-
-        return NoteUserDto.fromEntity(note, noteImages);
-    }
-
-    void onSaveNoteFailure(Note note, List<NoteImage> noteImages) {
-        noteRepository.delete(note);
-        for (NoteImage noteImage : noteImages) {
-            noteImageRepository.deleteById(noteImage.getId());
-            fileService.onSaveNoteFailure(noteImage.getImageName());
-        }
-    }
-
     public void deleteNote(User user, Long noteId) {
         Note note = noteRepository.findByIdAndUser(noteId, user)
                 .orElseThrow(() -> new CustomErrorException(ErrorCode.NoSuchNoteExistException));
@@ -101,22 +57,42 @@ public class NoteService {
         return NoteDetailDto.fromEntity(note, noteImages);
     }
 
-    /**
-     * 1. 업데이트 요청 받은 노트의 id와 User 객체와 매칭되는 노트가 실제로 존재하는지 확인
-     * 1-1. 존재하지 않으면 NoSuchNoteExistException 에러를 발생시키고 에러를 응답하여 로직을 종료한다.
-     * 2. 반복문을 통해 additionTargetFiles에 대해 다음을 반복한다
-     * 2-1. additionTargetFiles에 담긴 이미지 파일을 저장한다
-     * 2-2. additionTargetFiles에 담긴 이미지 파일의 정보를 데이터베이스 저장한다.
-     * 2-3. 1과 2에서 에러가 발생할 경우 현재까지 저장한 이미지 파일 및 파일의 정보를 삭제하고 에러를 발생시키고 에러를 에러를 응답하고 로직을 종료한다.
-     * 3. 반복문을 통해 noteUpdateRequestDto의 deletionTargetFiles에 대해 다음을 반복한다.
-     * 3-1. 노트 id와 삭제할 파일 이름을 이용해서 해당 파일이 존재하는지 확인한다. 존재하지 않는다면 다음 반복으로 넘어간다.
-     * 3-2. 노트 id와 삭제할 파일 이름을 이용해서 해당 파일을 삭제한다
-     * 3-3. 노트 id와 삭제할 파일 이름을 이용해서 해당 파일의 정보를 데이터베이스에서 삭제한다
-     * 3-4. 2과 3에서 에러가 발생할 경우 2의 반복 작업 및 현재까지의 이미지 삭제를 취소하고 에러를 발생시키고 에러를 에러를 응답하고 로직을 종료한다.
-     * 4. 노트 정보 업데이트
-     * 4-1. 노트 정보 업데이트에 실패하면 2와 3의 반복작업을 취소하고 에러를 발생시키고 에러를 에러를 응답하고 로직을 종료한다.
-     * 5. noteUpdateResponseDto를 반환한다.
-     */
+    public NoteUserDto saveNote(User user, NoteSaveRequestDto noteSaveRequestDto, List<MultipartFile> files) {
+        List<String> addedImages = new ArrayList<>();
+
+        // 노트 저장
+        Note note = noteRepository.save(new Note(
+                noteSaveRequestDto.getTitle(),
+                noteSaveRequestDto.getContent(),
+                user
+        ));
+
+        // 스토리지 및 데이터베이스에 노트 이미지 저장
+        if (files != null) {
+            for (MultipartFile file : files) {
+                if (!fileService.checkImageFile(file)) {
+                    fileService.deleteFiles(UploadPathType.IMAGE, addedImages);
+                    throw new CustomErrorException(ErrorCode.NoImageFileError);
+                }
+
+                try {
+                    String uniqueFileName = fileService.saveFiles(UploadPathType.IMAGE, file);
+                    NoteImage noteImage = noteImageRepository.save(new NoteImage(note, uniqueFileName));
+                    addedImages.add(noteImage.getImageName());
+                } catch (NullPointerException e) {
+                    fileService.deleteFiles(UploadPathType.IMAGE, addedImages);
+                    throw new CustomErrorException(ErrorCode.NoImageNameError);
+
+                } catch (Exception e) {
+                    fileService.deleteFiles(UploadPathType.IMAGE, addedImages);
+                    throw new CustomErrorException(ErrorCode.InternalServerError);
+                }
+            }
+
+        }
+
+        return NoteUserDto.fromEntity(note, noteImageRepository.findNoteImageByNote(note));
+    }
 
     public NoteUserDto updateNote(User user, Long noteId, NoteUpdateRequestDto noteUpdateRequestDto, List<MultipartFile> additionTargetFiles) {
         List<String> addedImages = new ArrayList<>();
@@ -137,7 +113,7 @@ public class NoteService {
                 }
 
                 try {
-                    String uniqueFileName = fileService.saveNoteImage(file);
+                    String uniqueFileName = fileService.saveFiles(UploadPathType.IMAGE, file);
                     NoteImage noteImage = noteImageRepository.save(new NoteImage(note, uniqueFileName));
                     addedImages.add(noteImage.getImageName());
                 } catch (NullPointerException e) {
