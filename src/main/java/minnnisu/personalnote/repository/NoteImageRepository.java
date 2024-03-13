935c0ce1a2f8bdb73f97b9ee274e354d6fe68fc5
minnnisu <mine52100@naver.com>
2024-03-13 19:36:38 +0900

    Add new query

--- src/main/java/minnnisu/personalnote/repository/NoteImageRepository.java
+++ src/main/java/minnnisu/personalnote/repository/NoteImageRepository.java
@@ -5,7 +5,11 @@ import minnnisu.personalnote.domain.NoteImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NoteImageRepository extends JpaRepository<NoteImage, Long> {
     List<NoteImage> findNoteImageByNote(Note note);
+
+    Optional<NoteImage> findByImageNameAndNote(String imageName, Note note);
+
 }
